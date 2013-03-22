81fdad50419a8d0a584f416ce147f1bac0207f67
Harald Walker <hwalker@tripolis.com>
2013-03-22 21:43:24 +0000

    USER_AGENT_UTILS-25 Added a Google Mobile bot pretending to be an iPhone as test case to see if it passes the test.

--- src/test/java/nl/bitwalker/useragentutils/OperatingSystemTest.java
+++ src/test/java/nl/bitwalker/useragentutils/OperatingSystemTest.java
@@ -141,7 +141,8 @@ public class OperatingSystemTest {
 	String[] iPhone4 = 
 	{
 			"Mozilla/5.0 (iPhone Simulator; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7",
-			"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7"
+			"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7",
+			"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_1 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B117 Safari/6531.22.7 (compatible; Googlebot-Mobile/2.1; +http://www.google.com/bot.html)" // Google bot pretending to be an iPhone
 	};
 	
 	String[] iPhone5 = 
