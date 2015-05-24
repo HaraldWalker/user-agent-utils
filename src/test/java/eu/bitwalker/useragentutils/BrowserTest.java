3603a9984b80e7fce4f62d14c26cc99fdbc35127
Francisco Collao Garate <pcollaog@gmail.com>
2015-05-24 16:19:17 -0300

    FIX: #71 add support to new bot:
    * facebook bot (facebookexternalhit)
    * bingbot and bingpreview
    * Seznam screenshot generator
    * Wordpress plugin mShots
    * Google Web Preview
    * Exabot
    * YandexMarket
    * Teoma Ask Jeeves' Web-indexing robot
    * ThumbSniper
    * Phantom.js bot

--- src/test/java/eu/bitwalker/useragentutils/BrowserTest.java
+++ src/test/java/eu/bitwalker/useragentutils/BrowserTest.java
@@ -167,9 +167,6 @@ public class BrowserTest {
             "Windows-RSS-Platform/2.0 (IE 11.0; Windows NT 6.1)"
     };
     
-    
-	
-	
 	String[] lotusNotes = new String[] {
 		"Mozilla/4.0 (compatible; Lotus-Notes/5.0; Windows-NT)",
 		"Mozilla/4.0 (compatible; Lotus-Notes/6.0; Windows-NT)"
@@ -458,7 +455,20 @@ public class BrowserTest {
 			"Googlebot-Image/1.0",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.8 (KHTML, like Gecko; Google Web Preview) Chrome/19.0.1084.36 Safari/536.8",
 			"Mediapartners-Google",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5 (Applebot/0.1)"
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5 (Applebot/0.1)",
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534+ (KHTML, like Gecko) BingPreview/1.0b",
+			"Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0; BingPreview/1.0b) like Gecko",
+			"Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 BingPreview/1.0b",
+			"Version/7.0 Mobile/11A465 Safari/9537.53 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+			"Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko; Google Web Preview) Version/5.1 Mobile/9A334 Safari/7534.48.3",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:10.0.12) Gecko/20100101 Firefox/21.0 WordPress.com mShots",
+			"Mozilla/5.0 (compatible; Seznam screenshot-generator 2.1; +http://fulltext.sblog.cz/screenshot/)",
+			"facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
+			"Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Exabot-Thumbnails)",
+			"Mozilla/5.0 (compatible; YandexMarket/1.0; +http://yandex.com/bots)",
+			"Mozilla/5.0 (compatible; Ask Jeeves/Teoma; +http://about.ask.com/en/docs/about/webmasters.shtml)",
+			"ThumbSniper (http://thumbsniper.com)",
+			"Phantom.js bot"
 	};
 	
 	String[] mobileBot = {
