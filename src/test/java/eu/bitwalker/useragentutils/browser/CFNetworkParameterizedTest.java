d9c7f410595ff98ceb4f34f94f2a0e40c91be1d3
Francisco Collao Garate <pcollaog@gmail.com>
2015-05-25 10:19:52 -0300

    CHG: change the test constructor for OperatingSystem

--- src/test/java/eu/bitwalker/useragentutils/browser/CFNetworkParameterizedTest.java
+++ src/test/java/eu/bitwalker/useragentutils/browser/CFNetworkParameterizedTest.java
@@ -7,13 +7,14 @@ import java.util.Collection;
 import org.junit.runners.Parameterized.Parameters;
 
 import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.OperatingSystem;
 
 public class CFNetworkParameterizedTest extends
 		AbstractUserAgentParameterizedTest {
 
 	public CFNetworkParameterizedTest(String userAgentValue,
 			Browser expectedBrowser, String browserVersionExpected,
-			String osExpected) {
+			OperatingSystem osExpected) {
 		super(userAgentValue, expectedBrowser, browserVersionExpected,
 				osExpected);
 	}
@@ -24,11 +25,11 @@ public class CFNetworkParameterizedTest extends
 
 				// CFNetworks
 				{ "MobileSafari/9537.53 CFNetwork/672.0.8 Darwin/14.0.0",
-						Browser.CFNETWORK, "672.0.8", "MAC_OS_X" },
+						Browser.CFNETWORK, "672.0.8", OperatingSystem.MAC_OS_X },
 				{ "MobileSafari/9537.53 CFNetwork/128 Darwin/14.0.0",
-						Browser.CFNETWORK, "128", "MAC_OS_X" },
+						Browser.CFNETWORK, "128", OperatingSystem.MAC_OS_X },
 				{ "MobileSafari/9537.53 CFNetwork/129.9 Darwin/14.0.0",
-						Browser.CFNETWORK, "129.9", "MAC_OS_X" },
+						Browser.CFNETWORK, "129.9", OperatingSystem.MAC_OS_X },
 
 		});
 	}
