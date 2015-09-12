de79d694b6d8e08385d42327e260405cfaed32e1
Harald Walker <hwalker@tripolis.com>
2015-09-12 11:45:39 +0200

    #122 Add new test string from reported issue
    Since unit test passed, no additional changes.

--- src/test/java/eu/bitwalker/useragentutils/BrowserTest.java
+++ src/test/java/eu/bitwalker/useragentutils/BrowserTest.java
@@ -89,7 +89,8 @@ public class BrowserTest {
 
 	String[] edgeClients = new String[] {
 			"Mozilla/5.0 (Windows NT 6.4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36 Edge/12.0",
-			"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240"
+			"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10532"
 	};
 	
 	String[] edgeMobileClients = new String[] {
